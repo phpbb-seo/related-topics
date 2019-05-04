77f46407b556e3ed371a9915b1f74694e25db833
Meisam Nobari <meis2m@yahoo.com>
2019-05-04 12:08:14 +0430

    release b1

---
+++ ext.php
@@ -0,0 +1,20 @@
+<?php
+/**
+*
+* @package No Duplicate phpBB SEO
+* @version $$
+* @copyright (c) 2019 www.phpbb-seo.org
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace phpbbseo\related;
+
+/**
+* ext Class
+* www.phpBB-SEO.org
+* @package No Duplicate phpBB SEO
+*/
+class ext extends \phpbb\extension\base
+{
+}
